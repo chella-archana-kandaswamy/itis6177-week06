3b7ba5df05a8fa4bab35c59c667ecb683b405dda
Chella Archana <archkand@gmail.com>
2020-02-26 21:33:49 -0500

    Mapped lambda function with server

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = 3000
-
+const axios = require('axios')
 app.use(cors())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+        axios.get('https://kbfafxfdz2.execute-api.us-east-1.amazonaws.com/one')
+        .then(resp=>{
+        res.send(resp.data.body);
+    
+})
+.catch(function(err){
+    console.log(err);
+})
+
+})
+    
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
